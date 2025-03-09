dec13120d6f5d98f301e352df152c5c1d86cbccd
cuong9565 <cuongmanhle0905@gmail.com>
2025-03-09 19:45:37 +0700

    Cuong - Login Form

---
+++ MiniMartSystem/src/LoginForm.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class LoginForm extends JFrame {
+    public LoginForm() {
+        setTitle("Đăng nhập");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
