d86130728d525d2f3ef85812846104b6f561c34b
baro1chuo <lychibao200t@gmail.com>
2025-03-17 13:10:45 +0700

    Bao

--- src/pnForm/pnImport.java
+++ src/pnForm/pnImport.java
@@ -1,16 +1,91 @@
 package pnForm;
 
 import Components.MyJButton;
+import Components.MyJLabel;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class pnImport extends JPanel{
-    JButton button = MyJButton.GetJButton(Font.PLAIN, 16, "#000000", "#FFFFFF", "#FFFFFF", "Nhập hàng", SwingConstants.CENTER, SwingConstants.CENTER);
+    //JButton button = MyJButton.GetJButton(Font.PLAIN, 16, "#000000", "#FFFFFF", "#FFFFFF", "Nhập hàng", SwingConstants.CENTER, SwingConstants.CENTER);
+    DefaultTableModel model = new DefaultTableModel();
+    DefaultTableModel model1 = new DefaultTableModel();
+    JButton btnRefresh = MyJButton.GetJButton(Font.PLAIN, 16, "#000000", "#FFFFFF", "#FFFFFF", "Làm mới", SwingConstants.CENTER, SwingConstants.CENTER);
+    JLabel lbID = MyJLabel.GetJLabel(Font.PLAIN,12,"#000000","Mã phiếu nhập",SwingConstants.LEFT, SwingConstants.CENTER);
+    JLabel lbCreate = MyJLabel.GetJLabel(Font.PLAIN,12,"#000000","Người tạo phiếu",SwingConstants.LEFT, SwingConstants.CENTER);
+    JLabel lbSupply = MyJLabel.GetJLabel(Font.PLAIN,12,"#000000","Nhà cung cấp",SwingConstants.LEFT, SwingConstants.CENTER);
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel tablePanel = new JPanel();
+    JPanel tablePanel1 = new JPanel();
+    JTextField txtSearch = new JTextField();
+    JTextField txtID = new JTextField();
+    JTextField txtCreate = new JTextField();
+    JComboBox cboSupplier = new JComboBox<>();
+    JTable tbProduct;
+    JTable tbImport;
+
     public pnImport() {
         setLayout(null);
-        setBackground(Color.decode("#FF00FF"));
-        button.setBounds(10,10,100,100);
-        add(button);
+        setBackground(Color.decode("#FFFFFF"));
+        Border border = BorderFactory.createLineBorder(Color.gray,1);
+        panel1.setBorder(BorderFactory.createTitledBorder(border,"Tìm kiếm"));
+        panel1.setBackground(Color.decode("#FFFFFF"));
+        panel1.setBounds(10,10,450,80);
+
+        txtSearch.setFont(new Font("Arial",Font.PLAIN,20));
+        txtSearch.setHorizontalAlignment(JTextField.CENTER);
+        txtSearch.setBounds(30,35,302,36);
+        txtSearch.setBorder(border);
+
+        btnRefresh.setBorder(border);
+        btnRefresh.setBounds(342,35,100,36);
+
+        panel2.setBackground(Color.decode("#FFFFFF"));
+        panel2.setLayout(new GridLayout(3,2,10,10));
+        panel2.add(lbID); panel2.add(txtID);
+        panel2.add(lbCreate); panel2.add(txtCreate);
+        panel2.add(lbSupply); panel2.add(cboSupplier);
+        panel2.setBounds(490,10,430,100);
+
+        String[] columns = {"Mã sản phẩm", "Tên sản phẩm", "Số lượng", "Đơn giá"};
+        model.setColumnIdentifiers(columns);
+        tbProduct = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(tbProduct);
+        //tbProduct.setBackground(Color.decode("#FFFFFF"));
+        scrollPane.getViewport().setBackground(Color.decode("#FFFFFF"));
+        tbProduct.setRowHeight(30);
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER); // Căn giữa
+        tbProduct.setDefaultRenderer(Object.class, centerRenderer);
+        tablePanel.setLayout(new GridLayout(1,1));
+        tablePanel.add(scrollPane);
+        tablePanel.setBounds(10,110,450,550);
+
+        String[] columns1 = {"STT", "Mã sản phẩm", "Tên sản phẩm", "Số lượng", "Đơn giá"};
+        model1.setColumnIdentifiers(columns1);
+        tbImport = new JTable(model1);
+        JScrollPane scrollPane1 = new JScrollPane(tbImport);
+        //tbProduct.setBackground(Color.decode("#FFFFFF"));
+        scrollPane1.getViewport().setBackground(Color.decode("#FFFFFF"));
+        tbImport.setRowHeight(30);
+        DefaultTableCellRenderer centerRenderer1 = new DefaultTableCellRenderer();
+        centerRenderer1.setHorizontalAlignment(SwingConstants.CENTER); // Căn giữa
+        tbImport.setDefaultRenderer(Object.class, centerRenderer);
+        tablePanel1.setLayout(new GridLayout(1,1));
+        tablePanel1.add(scrollPane1);
+        tablePanel1.setBounds(480,150,480,430);
+
+
+        add(tablePanel1);
+        add(tablePanel);
+        add(btnRefresh);
+        add(txtSearch);
+        add(panel1);
+        add(panel2);
+
     }
 }
--- src/pnForm/pnStatistic.java
+++ src/pnForm/pnStatistic.java
@@ -3,16 +3,61 @@ package pnForm;
 import Components.MyJButton;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class pnStatistic extends JPanel {
+    DefaultTableModel model = new DefaultTableModel();
+    //ArrayList<Thongke> list = new ArrayList<>();
     JButton button = MyJButton.GetJButton(Font.PLAIN, 16, "#000000", "#FFFFFF", "#FFFFFF", "Thống kê", SwingConstants.CENTER, SwingConstants.CENTER);
+    JPanel jPanelLayout = new JPanel();
+    JPanel jPanel1 = new JPanel();
+    JPanel jPanel2 = new JPanel();
+    JPanel jPanel3 = new JPanel();
+    JPanel jPanel4 = new JPanel();
+    JPanel jPanel5 = new JPanel();
+    JPanel jPanel6 = new JPanel();
+    JPanel tablePanel = new JPanel();
+    JTabbedPane tabPane = new JTabbedPane();
+    JTable tbStatistic;
+
+
     public pnStatistic() {
         setLayout(null);
-        setBackground(Color.decode("#FF00FF"));
-        button.setBounds(10,10,100,100);
-        add(button);
+        setBackground(Color.decode("#FFFFFF"));
+        jPanel1.setBackground(Color.decode("#45e7f3"));
+        jPanel2.setBackground(Color.decode("#f3a145"));
+        jPanel3.setBackground(Color.decode("#a1f345"));
+        jPanel4.setBackground(Color.decode("#FFFFFF"));
+        jPanel5.setBackground(Color.decode("#FFFFFF"));
+        jPanel6.setBackground(Color.decode("#FFFFFF"));
+
+        jPanelLayout.setLayout(new GridLayout(1,3, 10,10));
+        //jPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        tabPane.add("Sản phẩm",jPanel4);
+        tabPane.add("Phiếu",jPanel5);
+        tabPane.add("Tài khoản",jPanel6);
+        tabPane.setBounds(10,220,950,150);
+
+        jPanelLayout.add(jPanel1);
+        jPanelLayout.add(jPanel2);
+        jPanelLayout.add(jPanel3);
+        jPanelLayout.setBounds(10,10,950,200);
+
+        //tablePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        String[] columns = {"STT", "Mã sản phẩm", "Tên sản phẩm", "Số lượng nhập", "Số lượng xuất"};
+        model.setColumnIdentifiers(columns);
+        tbStatistic = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(tbStatistic);
+        tablePanel.setBackground(Color.decode("#FFFFFF"));
+        tablePanel.setLayout(new GridLayout(1,1));
+        tablePanel.add(scrollPane);
+        tablePanel.setBounds(10,380,950,350);
+
+        add(tablePanel);
+        add(tabPane);
+        add(jPanelLayout);
     }
 }
