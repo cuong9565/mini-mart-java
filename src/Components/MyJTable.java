78b549993f0de43fdceebf784e97d5dd4b1820c7
cuong9565 <cuongmanhle0905@gmail.com>
2025-03-21 18:20:44 +0700

    Cuong update JTable

---
+++ src/Components/MyJTable.java
@@ -0,0 +1,39 @@
+package Components;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class MyJTable extends JTable {
+    public DefaultTableModel dftbModel;
+    public JScrollPane scrPn;
+    public MyJTable(String header[]) {
+        dftbModel = new DefaultTableModel(header, 0){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        setModel(dftbModel);
+
+        // Set cho header
+        getTableHeader().setFont(new Font("Roboto", Font.BOLD, 14));
+        getTableHeader().setBackground(MyColor.HoverBlue);
+        getTableHeader().setForeground(MyColor.Black);
+        getTableHeader().setReorderingAllowed(false);
+        getTableHeader().setBorder(null);
+        getTableHeader().setPreferredSize(new Dimension(this.getTableHeader().getWidth(), 30));
+
+        // Set cho content
+        setFont(new Font("Roboto", Font.PLAIN, 12));
+        setBackground(MyColor.White);
+        setForeground(MyColor.Black);
+        setRowHeight(20);
+
+        scrPn = new JScrollPane(this);
+    }
+    public void addRow(Object[] rows) {
+        dftbModel.addRow(rows);
+    }
+}
--- src/pnForm/pnSupplier.java
+++ src/pnForm/pnSupplier.java
@@ -1,13 +1,15 @@
 package pnForm;
 
 import Components.*;
-
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+
 
 public class pnSupplier extends JPanel {
     JPanel pnHeader = new MyJPanel(MyColor.White);
+    JPanel pnFooter = new MyJPanel(MyColor.White);
     JPanel pnFunc = new MyJPanel(MyColor.White, "Chức năng");
     JPanel pnSearch = new MyJPanel(MyColor.White, "Tìm kiếm");
     JButton btnAdd = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#4CAF50"), Color.decode("#7ED482"), "Thêm", SwingConstants.CENTER, SwingConstants.CENTER);
@@ -15,7 +17,9 @@ public class pnSupplier extends JPanel {
     JButton btnDelete = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#F44336"), Color.decode("#FF7568"), "Xóa", SwingConstants.CENTER, SwingConstants.CENTER);
     JButton btnRefresh = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#2196F3"), Color.decode("#64B5F6"), "Làm mới", SwingConstants.CENTER, SwingConstants.CENTER);
     JTextField tfSearch = new MyJTextFieldInput(Font.PLAIN, 14, true);
-    JComboBox<String>cbSearch = new MyJComboBox<>(new String[]{"Tất cả"}, 12);
+    JComboBox<String>cbSearch = new MyJComboBox<>(new String[]{"Tất cả", "1", "2", "3", "4", "5"}, 12);
+
+    MyJTable tbSupplier = new MyJTable(new String[]{"1", "2", "3", "4", "5"});
 
     public pnSupplier(JFrame frame) {
         setLayout(null);
@@ -29,9 +33,18 @@ public class pnSupplier extends JPanel {
         btnDelete.setBounds(175,19,70,70);
 
         pnSearch.setBounds(455,0,515,100);
-        btnRefresh.setBounds(855,29,100,40);
-        tfSearch.setBounds(645, 29, 200, 40);
-        cbSearch.setBounds(475, 29, 150, 40);
+        cbSearch.setBounds(475, 30, 150, 40);
+        tfSearch.setBounds(645, 30, 200, 40);
+        btnRefresh.setBounds(855,30,100,40);
+
+        pnFooter.setBounds(0,110,970, 630);
+        tbSupplier.scrPn.setBounds(0,110,970,630);
+
+        tbSupplier.addRow(new Object[]{"1", "2", "3", "4", "5"});
+        tbSupplier.addRow(new Object[]{"1", "2", "3", "4", "5"});
+        tbSupplier.addRow(new Object[]{"1", "2", "3", "4", "5"});
+        tbSupplier.addRow(new Object[]{"1", "2", "3", "4", "5"});
+        tbSupplier.addRow(new Object[]{"1", "2", "3", "4", "5"});
 
         add(btnAdd);
         add(btnEdit);
@@ -42,5 +55,8 @@ public class pnSupplier extends JPanel {
         add(pnFunc);
         add(pnSearch);
         add(pnHeader);
+
+        add(tbSupplier.scrPn);
+        add(pnFooter);
     }
 }
