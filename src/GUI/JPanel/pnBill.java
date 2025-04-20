2168f1f979c2f7cae4daf10fa68fd85d5fb53b63
TphuSGU <thanhphu142857@gmail.com>
2025-04-20 13:25:31 +0700

    P PDF bill

--- src/GUI/JPanel/pnBill.java
+++ src/GUI/JPanel/pnBill.java
@@ -4,14 +4,13 @@ import BUS.BillBUS;
 import BUS.BillInfoBUS;
 import Components.*;
 import DTO.*;
-import GUI.JFrame.fManage;
-
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.io.FileOutputStream;
+import java.util.List;
 public class pnBill extends JPanel {
     JPanel pnHeader = new MyJPanel(MyColor.White);
     JPanel pnFooter = new MyJPanel(MyColor.White);
@@ -19,12 +18,12 @@ public class pnBill extends JPanel {
     JPanel pnSearch = new MyJPanel(MyColor.White, "Tìm kiếm");
     JButton btnDelete = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#F44336"), Color.decode("#FF7568"), "Xóa", SwingConstants.CENTER, SwingConstants.CENTER);
     JButton btnOut = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#2196F3"), Color.decode("#64B5F6"), "<html>Xuất<br>Exel</html>", SwingConstants.CENTER, SwingConstants.CENTER);
+    JButton btnPDF = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#2196F3"), Color.decode("#64B5F6"), "<html>IN<br>PDF</html>", SwingConstants.CENTER, SwingConstants.CENTER);
     JButton btnDetail = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#2196F3"), Color.decode("#64B5F6"), "<html>Xuất<br>Chi tiết</html>", SwingConstants.CENTER, SwingConstants.CENTER);
     JButton btnRefresh = new MyJButton(Font.BOLD, 16, Color.decode("#FFFFFF"), Color.decode("#2196F3"), Color.decode("#64B5F6"), "Làm mới", SwingConstants.CENTER, SwingConstants.CENTER);
     JTextField tfSearch = new MyJTextFieldInput(Font.PLAIN, 14, true);
     JComboBox<String>cbSearch = new MyJComboBox<>(new String[]{"Mã HĐ", "Ngày tạo", "Thành tiền", "Trạng thái"}, 12);
     MyJTable tbBill = new MyJTable(new String[]{"Mã HĐ", "Ngày tạo", "Thành tiền", "Trạng thái"}, new int[]{100, 100, 150, 150}, new int[]{}, new int[]{});
-
     MyJTable tbBillInfo = new MyJTable(new String[]{"Mã SP", "Tên SP" , "Đơn giá", "Số lượng", "Giảm giá", "Đơn vị ", "Thành tiền"}, new int[]{50, 125, 125, 75, 75, 75, 125}, new int[]{1, 5}, new int[]{});
     JPanel pnBill = new MyJPanel(MyColor.White, "Thông tin chi tiết hóa đơn");
     JLabel lbStaff = new MyJLabel(Font.PLAIN, 14, MyColor.Black, "<html><u>Không có thông tin nhân viên</u></html>", SwingConstants.LEFT, SwingConstants.CENTER);
@@ -39,10 +38,11 @@ public class pnBill extends JPanel {
 
         // region setBounds
         pnHeader.setBounds(0,0,1170, 90);
-        pnFunc.setBounds(0,0,230,90);
+        pnFunc.setBounds(0,0,300,90);
         btnDelete.setBounds(15,20,60,60);
         btnOut.setBounds(85,20,60,60);
         btnDetail.setBounds(155,20,60,60);
+        btnPDF.setBounds(225,20,60,60);
         pnSearch.setBounds(670,0,500,90);
         cbSearch.setBounds(685, 30, 150, 30);
         tfSearch.setBounds(845, 30, 200, 30);
@@ -128,6 +128,149 @@ public class pnBill extends JPanel {
             public void removeUpdate(DocumentEvent e) {textChange();}
             public void changedUpdate(DocumentEvent e) {textChange();}
         });
+
+        btnPDF.addActionListener(e -> {
+            int i = tbBill.getSelectedRow();
+            if(i >= 0){
+                try {
+                    int id = Integer.parseInt(tbBill.getFirstColumn(i));
+                    BillDTO bill = BillBUS.getInstance().getBillById(id);
+                    List<BillInfoDTO> billInfos = BillInfoBUS.getInstance().loadByIdBill(id);
+
+                    JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setDialogTitle("Chọn nơi lưu hóa đơn");
+                    fileChooser.setSelectedFile(new java.io.File("HoaDon_" + id + ".pdf"));
+                    int userSelection = fileChooser.showSaveDialog(thisPanel);
+                    if (userSelection == JFileChooser.APPROVE_OPTION) {
+                        java.io.File fileToSave = fileChooser.getSelectedFile();
+                        String fileName = fileToSave.getAbsolutePath();
+                        if (!fileName.toLowerCase().endsWith(".pdf")) {
+                            fileName += ".pdf";
+                        }
+
+                        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+                        com.itextpdf.text.pdf.PdfWriter.getInstance(document, new FileOutputStream(fileName));
+                        document.open();
+                        // Font Unicode từ Arial
+                        String fontPath = "lib/arial.ttf";
+                        com.itextpdf.text.pdf.BaseFont baseFont = com.itextpdf.text.pdf.BaseFont.createFont(fontPath,
+                                com.itextpdf.text.pdf.BaseFont.IDENTITY_H, com.itextpdf.text.pdf.BaseFont.EMBEDDED);
+                        // Các loại font
+                        com.itextpdf.text.Font fontTitle = new com.itextpdf.text.Font(baseFont, 18, com.itextpdf.text.Font.BOLD);
+                        com.itextpdf.text.Font fontHeader = new com.itextpdf.text.Font(baseFont, 14, com.itextpdf.text.Font.BOLD);
+                        com.itextpdf.text.Font fontNormal = new com.itextpdf.text.Font(baseFont, 12);
+                        com.itextpdf.text.Font fontBold = new com.itextpdf.text.Font(baseFont, 12, com.itextpdf.text.Font.BOLD);
+                        // Header hóa đơn
+                        com.itextpdf.text.Paragraph storeName = new com.itextpdf.text.Paragraph("MINI MART", fontTitle);
+                        storeName.setAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        document.add(storeName);
+                        com.itextpdf.text.Paragraph invoiceTitle = new com.itextpdf.text.Paragraph("HÓA ĐƠN BÁN HÀNG", fontHeader);
+                        invoiceTitle.setAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        document.add(invoiceTitle);
+                        // Thông tin chung
+                        com.itextpdf.text.Paragraph billInfo = new com.itextpdf.text.Paragraph();
+                        billInfo.add(new com.itextpdf.text.Chunk("Mã hóa đơn: ", fontBold));
+                        billInfo.add(new com.itextpdf.text.Chunk(String.valueOf(bill.getId()), fontNormal));
+                        billInfo.add(new com.itextpdf.text.Chunk("\nNgày lập: ", fontBold));
+                        billInfo.add(new com.itextpdf.text.Chunk(bill.getDateCreate().toString(), fontNormal));
+                        billInfo.add(new com.itextpdf.text.Chunk("\nNhân viên: ", fontBold));
+                        billInfo.add(new com.itextpdf.text.Chunk(bill.getStaff().getLastName() + " " + bill.getStaff().getFirstName(), fontNormal));
+                        billInfo.add(new com.itextpdf.text.Chunk(" - SĐT: " + bill.getStaff().getPhone(), fontNormal));
+                        if(bill.getCustomer().getId() != 0) {
+                            billInfo.add(new com.itextpdf.text.Chunk("\nKhách hàng: ", fontBold));
+                            billInfo.add(new com.itextpdf.text.Chunk(bill.getCustomer().getLastName() + " " + bill.getCustomer().getFirstName(), fontNormal));
+                            billInfo.add(new com.itextpdf.text.Chunk(" - SĐT: " + bill.getCustomer().getPhone(), fontNormal));
+                        }
+                        document.add(billInfo);
+                        // Danh sách sản phẩm
+                        com.itextpdf.text.Paragraph productTitle = new com.itextpdf.text.Paragraph("\nDANH SÁCH SẢN PHẨM", fontBold);
+                        productTitle.setAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        document.add(productTitle);
+                        com.itextpdf.text.pdf.PdfPTable table = new com.itextpdf.text.pdf.PdfPTable(5);
+                        table.setWidthPercentage(100);
+                        table.setWidths(new float[]{3f, 1f, 2f, 1f, 2f});
+                        table.setSpacingBefore(10f);
+                        table.setSpacingAfter(10f);
+                        // Header table
+                        com.itextpdf.text.pdf.PdfPCell cell;
+                        cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase("Tên SP", fontBold));
+                        cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        cell.setBackgroundColor(new com.itextpdf.text.BaseColor(220, 220, 220));
+                        table.addCell(cell);
+                        cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase("SL", fontBold));
+                        cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        cell.setBackgroundColor(new com.itextpdf.text.BaseColor(220, 220, 220));
+                        table.addCell(cell);
+                        cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase("Đơn giá", fontBold));
+                        cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        cell.setBackgroundColor(new com.itextpdf.text.BaseColor(220, 220, 220));
+                        table.addCell(cell);
+                        cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase("Giảm", fontBold));
+                        cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        cell.setBackgroundColor(new com.itextpdf.text.BaseColor(220, 220, 220));
+                        table.addCell(cell);
+                        cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase("Thành tiền", fontBold));
+                        cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        cell.setBackgroundColor(new com.itextpdf.text.BaseColor(220, 220, 220));
+                        table.addCell(cell);
+                        // Content table
+                        for(BillInfoDTO info : billInfos){
+                            Object[] data = info.getSellObjects();
+
+                            cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase(String.valueOf(data[1]), fontNormal));
+                            cell.setPadding(5);
+                            table.addCell(cell);
+
+                            cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase(String.valueOf(data[3]), fontNormal));
+                            cell.setPadding(5);
+                            cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                            table.addCell(cell);
+
+                            cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase(String.valueOf(data[2]) + "đ", fontNormal));
+                            cell.setPadding(5);
+                            cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_RIGHT);
+                            table.addCell(cell);
+
+                            cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase(String.valueOf(data[4]) + "%", fontNormal));
+                            cell.setPadding(5);
+                            cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                            table.addCell(cell);
+
+                            cell = new com.itextpdf.text.pdf.PdfPCell(new com.itextpdf.text.Phrase(String.valueOf(data[6]) + "đ", fontNormal));
+                            cell.setPadding(5);
+                            cell.setHorizontalAlignment(com.itextpdf.text.Element.ALIGN_RIGHT);
+                            table.addCell(cell);
+                        }
+                        document.add(table);
+                        // Tổng thanh toán
+                        com.itextpdf.text.Paragraph total = new com.itextpdf.text.Paragraph();
+                        total.add(new com.itextpdf.text.Chunk("\nTổng tiền: ", fontBold));
+                        total.add(new com.itextpdf.text.Chunk(bill.getPrice() + " VNĐ", fontBold));
+                        total.setAlignment(com.itextpdf.text.Element.ALIGN_RIGHT);
+                        if(bill.getOfferBill().getId() != 0) {
+                            total.add(new com.itextpdf.text.Chunk("\nGiảm giá: ", fontBold));
+                            total.add(new com.itextpdf.text.Chunk(bill.getOfferBill().getDiscount() + "%", fontBold));
+                            double finalAmount = bill.getPrice() * (100 - bill.getOfferBill().getDiscount()) / 100;
+                            total.add(new com.itextpdf.text.Chunk("\nThành tiền: ", fontBold));
+                            total.add(new com.itextpdf.text.Chunk(finalAmount + " VNĐ", fontBold));
+                        }
+                        document.add(total);
+                        // Footer
+                        com.itextpdf.text.Paragraph thankYou = new com.itextpdf.text.Paragraph("\n\nCẢM ƠN QUÝ KHÁCH ! ", fontHeader);
+                        thankYou.setAlignment(com.itextpdf.text.Element.ALIGN_CENTER);
+                        document.add(thankYou);
+                        document.close();
+
+                        JOptionPane.showMessageDialog(thisPanel, "Xuất file PDF thành công:\n" + fileName);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(thisPanel, "Lỗi khi tạo PDF: " + ex.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(thisPanel, "Vui lòng chọn hóa đơn để in", "Thông báo", JOptionPane.WARNING_MESSAGE);
+            }
+        });
         // endregion
 
         // region add
@@ -137,6 +280,7 @@ public class pnBill extends JPanel {
         add(btnDelete);
         add(btnOut);
         add(btnDetail);
+        add(btnPDF);
         add(pnFunc);
         add(btnRefresh);
         add(cbSearch);
